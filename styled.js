7f64829d79dad4d7dad6397d721b809133c213d2
Denilson Cesar <denilson456987@gmail.com>
2019-09-28 12:24:07 -0300

    animação cartão de credito

---
+++ styled.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView.attrs({
+    showsHorizontalScrollIndicator: false,
+    horizontal: true
+})`
+    background-color: #fff;
+    margin-bottom: 8px;
+    border-radius: 10px;
+    margin: 5px;  
+    margin-right: 10px;
+`;
